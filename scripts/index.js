0087e8aef3c4e1a66470ea93d60c9b822e21c377
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2022-03-18 14:20:56 +0100

    menu changes

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ $(function () {
 var loadedCount = 0;
 var displayed = false;
 var can_display = false;
-const TO_LOAD_COUNT = 11;
+const TO_LOAD_COUNT = 6;
 const SCROLL_SPEED_SECONDS = 1;
 
 // lista svih animacija koje se koriste (potrebna da bi se znalo koje animacije treba ukloniti)
@@ -29,72 +29,57 @@ const animationList = [
     'slide-gesture-in-overlay'
 ];
 
+function isMobile() {
+    let agent = navigator.userAgent||navigator.vendor||window.opera;
+    return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(agent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| ||a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(agent.substr(0,4));
+}
+
 function loadImages() {
-    $('<img/>').attr('src', '../content/backgrounds/index/HOME(DESKTOP).jpg').on('load', function() {
-        $(this).remove();
-        /* $('.yachting .bg').css('background-image', 'url(../content/backgrounds/index/yachting_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/travel_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.travel .bg').css('background-image', 'url(../content/backgrounds/index/travel_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/entertainment_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.entertainment .bg').css('background-image', 'url(../content/backgrounds/index/entertainment_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/sports_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.sports .bg').css('background-image', 'url(../content/backgrounds/index/sports_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/transport_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.transport .bg').css('background-image', 'url(../content/backgrounds/index/transport_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/celebrations_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.celebrations .bg').css('background-image', 'url(../content/backgrounds/index/celebrations_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/luxury_items_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.luxury_items .bg').css('background-image', 'url(../content/backgrounds/index/luxury_items_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/wellness_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.wellness .bg').css('background-image', 'url(../content/backgrounds/index/wellness_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/events_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.events .bg').css('background-image', 'url(../content/backgrounds/index/events_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/backgrounds/index/networking_bg.jpg').on('load', function() {
-        $(this).remove();
-        /* $('.networking .bg').css('background-image', 'url(../content/backgrounds/index/networking_bg.jpg)'); */
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
-    $('<img/>').attr('src', '../content/images/7D_logo.png').on('load', function() {
-        $(this).remove();
-        loadedCount++;
-        if (loadedCount == TO_LOAD_COUNT) can_display = true;
-    });
+
+    if(isMobile()){
+        $('<img/>').attr('src', '../content/backgrounds/index/AS SEEN ON (HOME).png').on('load', function() {
+            $(this).remove();
+            /* $('.yachting .bg').css('background-image', 'url(../content/backgrounds/index/yachting_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+        $('<img/>').attr('src', '../content/backgrounds/index/HOME(MOBILE).jpg').on('load', function() {
+            $(this).remove();
+            /* $('.travel .bg').css('background-image', 'url(../content/backgrounds/index/travel_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+        $('<img/>').attr('src', '../content/backgrounds/index/REFERENCES (MOBILE).png').on('load', function() {
+            $(this).remove();
+            /* $('.entertainment .bg').css('background-image', 'url(../content/backgrounds/index/entertainment_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+    }
+    else{
+        $('<img/>').attr('src', '../content/backgrounds/index/HOME(DESKTOP).jpg').on('load', function() {
+            $(this).remove();
+            /* $('.yachting .bg').css('background-image', 'url(../content/backgrounds/index/yachting_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+        $('<img/>').attr('src', '../content/backgrounds/index/As Seen On(DESKTOP).png').on('load', function() {
+            $(this).remove();
+            /* $('.travel .bg').css('background-image', 'url(../content/backgrounds/index/travel_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+        $('<img/>').attr('src', '../content/backgrounds/index/REFERENCES (DESKTOP).png').on('load', function() {
+            $(this).remove();
+            /* $('.entertainment .bg').css('background-image', 'url(../content/backgrounds/index/entertainment_bg.jpg)'); */
+            loadedCount++;
+            if (loadedCount == TO_LOAD_COUNT) can_display = true;
+        });
+    
+    }
+    
+   
+
 }
 
 function displaySite() {
@@ -107,7 +92,7 @@ function displaySite() {
         loading.css("position", "absolute");
         loading.css("visibility", "hidden");
 
-        let menu_items = $('.menu_items').eq(0);
+        let menu_items = $('.menudiv').eq(0);
         menu_items.css("position", "static");
         menu_items.css("visibility", "visible");
 
@@ -282,6 +267,8 @@ function showOverlay() {
 
         $('.header').css('visibility', 'hidden');
         menu.css('visibility', 'visible');
+        $('.footer').css('visibility', 'hidden');
+        $('.reserved').css('visibility', 'hidden');
 
         /* show overlay */
         setTimeout(function() {
@@ -290,10 +277,11 @@ function showOverlay() {
             overlay.css('visibility', 'visible');
     
             let overlay_menu_links = $('#overlay .menu_items a');
+            overlay_menu_links.push($('#nftMobile')[0]);
             overlay_menu_links.css('transform', 'translateX(0)');
             overlay_menu_links.css('opacity', '1');
             for (let i = 0; i < overlay_menu_links.length; i++) {
-                delay = (i / 7 + 0.5) + 's';
+                delay = (i / 8 + 0.5) + 's';
                 overlay_menu_links[i].style.animation = "slide-in-overlay-items 0.25s ease-out " + delay + " both";
             }
         }, 125);
@@ -336,12 +324,15 @@ function hideOverlay() {
     overlay.css('z-index', '-1');
 
     let overlay_menu_links = $('#overlay .menu_items a');
+    overlay_menu_links.push($('#nftMobile')[0]);
     for (let i = 0; i < overlay_menu_links.length; i++) {
         overlay_menu_links.eq(i)[0].style.animation = "";
     }
 
     /* show content */
     $('.header').css('visibility', 'visible');
+    $('.footer').css('visibility', 'visible');
+    $('.reserved').css('visibility', 'visible');
 
     let nav = $('.page_number').eq(i);
     removeAnimations(nav[0]);
